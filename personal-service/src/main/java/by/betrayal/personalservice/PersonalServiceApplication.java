d9215d0944aa45995602ce156136e5639ab05be3
betrayal <vogistv@gmail.com>
2024-03-06 23:41:34 +0700

    create personal-service and configured file for create database

---
+++ personal-service/src/main/java/by/betrayal/personalservice/PersonalServiceApplication.java
@@ -0,0 +1,15 @@
+package by.betrayal.personalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PersonalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalServiceApplication.class, args);
+	}
+
+}
