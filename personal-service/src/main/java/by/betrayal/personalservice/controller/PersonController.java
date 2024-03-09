394c2f46d3a917efef8c8b269c9ab49abcd64814
betrayal <vogistv@gmail.com>
2024-03-09 19:56:08 +0700

    add logging controller

--- personal-service/src/main/java/by/betrayal/personalservice/controller/PersonController.java
+++ personal-service/src/main/java/by/betrayal/personalservice/controller/PersonController.java
@@ -4,8 +4,10 @@ import by.betrayal.personalservice.dto.person.PersonCreateDto;
 import by.betrayal.personalservice.dto.person.PersonFullDto;
 import by.betrayal.personalservice.dto.person.PersonUpdateDto;
 import by.betrayal.personalservice.service.PersonService;
+import by.betrayal.personalservice.utils.LoggerHelper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -22,42 +24,42 @@ public class PersonController {
 
     @GetMapping
     public List<PersonFullDto> findAll() {
-        log.info("--- Start request in controller `PersonController` - findAll ---");
+        log.info(LoggerHelper.createLogStart("PersonController", "findAll", ""));
         var items = service.findAll();
-        log.info("--- End request in controller `PersonController` - findAll with result {} ---", items.size());
+        log.info(LoggerHelper.createLogEnd("PersonController", "findAll", "with result size {}"), items.size());
         return items;
     }
 
     @GetMapping( value = "{id}")
     public PersonFullDto findById(@PathVariable Long id) {
-        log.info("--- Start request in controller `PersonController` - findById with id {} ---", id);
+        log.info(LoggerHelper.createLogIdStart("PersonController", "findById", id));
         var item = service.findById(id);
-        log.info("--- End request in controller `PersonController` - findById result success ---");
+        log.info(LoggerHelper.createLogEnd("PersonController", "findById", "result success"));
         return item;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public PersonFullDto create(@Valid @RequestBody PersonCreateDto dto) {
-        log.info("--- Start request in controller `PersonController` - create ---");
+        log.info(LoggerHelper.createLogStart("PersonController", "create", dto.toString()));
         var item = service.create(dto);
-        log.info("--- End request in controller `PersonController` - create result id {} ---", item.getId());
+        log.info(LoggerHelper.createLogEnd("PersonController", "create", "object created with id {}"), item.getId());
         return item;
     }
 
     @PutMapping
     public PersonFullDto update(@Valid @RequestBody PersonUpdateDto dto) {
-        log.info("--- Start request in controller `PersonController` - update {}---", dto.getId());
+        log.info(LoggerHelper.createLogStart("PersonController", "update", dto.toString()));
         var item = service.update(dto);
-        log.info("--- End request in controller `PersonController` - update result id {} ---", item.getId());
+        log.info(LoggerHelper.createLogEnd("PersonController", "update", item.toString()));
         return item;
     }
 
     @DeleteMapping("{id}")
     public PersonFullDto delete(@PathVariable Long id) {
-        log.info("--- Start request in controller `PersonController` - delete {}---", id);
+        log.info(LoggerHelper.createLogIdStart("PersonController", "delete", id));
         var item = service.delete(id);
-        log.info("--- End request in controller `PersonController` - delete result id {} ---", item.getId());
+        log.info(LoggerHelper.createLogEnd("PersonController", "delete", item.toString()));
         return item;
     }
 }
--- personal-service/src/main/java/by/betrayal/personalservice/exception/Handler.java
+++ personal-service/src/main/java/by/betrayal/personalservice/exception/Handler.java
@@ -1,6 +1,8 @@
 package by.betrayal.personalservice.exception;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
