16fde45617b6822588d2291e98c28bd753e3c845
betrayal <vogistv@gmail.com>
2024-03-17 10:35:04 +0700

    add email sender

--- notification-service/src/main/java/by/betrayal/notificationservice/service/impl/NotificationServiceImpl.java
+++ notification-service/src/main/java/by/betrayal/notificationservice/service/impl/NotificationServiceImpl.java
@@ -2,24 +2,34 @@ package by.betrayal.notificationservice.service.impl;
 
 
 import by.betrayal.notificationservice.entity.PersonEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
 
 @Service
 @EnableRabbit
 public class NotificationServiceImpl {
 
+    @Value("${spring.mail.username}")
+    private String publisherMail;
+
+    @Autowired
+    private JavaMailSender sender;
 
     @RabbitListener(queues = {"${spring.rabbitmq.queue.name}"})
     public void receiveMessage(PersonEntity person) {
-        System.out.println(person.getFirstName());
+        var message = new SimpleMailMessage();
+        message.setFrom(publisherMail);
+        message.setTo("officialleafall@gmail.com");
+        String bodyMessage = String.format("Account be registered on with data \nfirstName %s", person.getFirstName());
+        message.setText(bodyMessage);
+        message.setSubject("New registered account");
+
+        sender.send(message);
     }
 }
